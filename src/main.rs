3a8f8e6771f63174dfd6a7c4fecaaa8c8efd9854
Sergey <freak12techno@gmail.com>
2024-07-08 18:34:39 +0300

    add validators endpoint

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ extern crate rocket;
 use rocket::serde::json::{json, Value};
 use rocket::State;
 use clap::Parser;
+use futures::TryStreamExt;
 
 use penumbra_proto::{
     core::app::v1::{
@@ -21,7 +22,10 @@ use penumbra_proto::{
         tendermint_proxy_service_client::TendermintProxyServiceClient, GetStatusRequest,
     },
 };
-use penumbra_stake::{IdentityKey, Uptime};
+use penumbra_stake::{
+    IdentityKey, Uptime,
+    validator::{self, Info, Status, Validator, ValidatorToml, BondingState, State as ValidatorState},
+};
 
 use tonic::transport::{Channel, ClientTlsConfig};
 
@@ -34,6 +38,86 @@ struct Args {
 
     #[arg(short, long, default_value_t = 8000)]
     port: i32,
+}
+
+#[get("/cosmos/staking/v1beta1/validators?<status>")]
+async fn validators(status: Option<String>, args: &State<Args>) -> Value {
+    let channel = Channel::from_shared(args.node.to_string())
+        .unwrap()
+        .tls_config(ClientTlsConfig::new())
+        .unwrap()
+        .connect()
+        .await
+        .unwrap();
+
+    let mut client = StakeQueryServiceClient::new(channel);
+
+    let validators: Vec<validator::Info> = client
+        .validator_info(ValidatorInfoRequest {
+            show_inactive: true,
+            ..Default::default()
+        })
+        .await
+        .unwrap()
+        .into_inner()
+        .try_collect::<Vec<_>>()
+        .await
+        .unwrap()
+        .into_iter()
+        .map(TryInto::try_into)
+        .collect::<Result<Vec<validator::Info>, _>>()
+        .unwrap();
+
+    let mut result: Vec<_> = vec![];
+    for validator in validators {
+        let validator_status = match validator.status.bonding_state {
+            BondingState::Bonded => "BOND_STATUS_BONDED",
+            BondingState::Unbonding { unbonds_at_height: _ } => "BOND_STATUS_UNBONDING",
+            BondingState::Unbonded => "BOND_STATUS_UNBONDED",
+        };
+
+        if !status.is_none() && status != Some(validator_status.to_owned()) {
+            continue;
+        }
+
+        result.push(json!({
+            "operator_address": validator.validator.identity_key.to_string(),
+            "consensus_pubkey": {
+                "@type": "/cosmos.crypto.ed25519.PubKey",
+                "key": base64::encode(validator.validator.consensus_key.to_bytes()),
+            },
+            "jailed": validator.status.state == ValidatorState::Jailed,
+            "status": validator_status,
+            "tokens": validator.status.voting_power.value().to_string(),
+            "delegator_shares": validator.status.voting_power.value().to_string(),
+            "description": {
+                "moniker": validator.validator.name,
+                "identity": "",
+                "website": validator.validator.website,
+                "security_contact": "",
+                "details": validator.validator.description,
+            },
+            "unbonding_height": "0", // TODO
+            "unbonding_time": "1970-01-01T00:00:00Z", // TODO
+            "commission": {
+                "commission_rates": {
+                    "rate": "0.05",
+                    "max_rate": "1.0",
+                    "max_change_rate": "1.0"
+                },
+                "update_time": "2023-08-04T06:00:00.000000000Z" // TODO
+            },
+            "min_self_delegation": "0"
+        }));
+    }
+
+    json!({
+        "validators": result,
+        "pagination": {
+            "next_key": null,
+            "total": result.len().to_string()
+        }
+    })
 }
 
 #[get("/cosmos/slashing/v1beta1/params")]
@@ -157,6 +241,7 @@ fn rocket() -> _ {
         .mount(
             "/",
             routes![
+                validators,
                 staking_params,
                 slashing_params,
                 signing_info,

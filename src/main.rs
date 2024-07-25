ad4c1e3a401320044e6b574f1d3c8085d35dbdf7
Keefer Taylor | Tessellated <keefer@tessellated.io>
2024-07-25 09:28:40 -0700

    small change to test caching on ci

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #[macro_use]
 extern crate rocket;
 
+// This change will break the cache
+
 use penumbra_proto::util::tendermint_proxy::v1::SyncInfo;
 use rocket::response::status;
 use rocket::serde::json::{Json, json, Value};
